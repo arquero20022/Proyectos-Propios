03afc017c728768a4f4a938d39668b15c23ffb6a
arquero20022 <60667813+arquero20022@users.noreply.github.com>
2023-07-16 20:45:37 +0200

    Delete Programacion/Python directory

--- Programacion/Python/DescuentoZoo.py
+++
@@ -1,7 +0,0 @@
-print("Bienvenido al zoologico")
-edad = int(input("Digame se edad: "))
-tipoDeCarnet = (input("que carnet tiene(E,P,F,N)"))
-if (25 < edad < 35 and tipoDeCarnet == "E") or (edad < 10) or (edad > 65 and tipoDeCarnet == "P") or tipoDeCarnet == "F":
-    print("Se le aplica el descuento del 25% ")
-else:
-    print("no se le aplica na de na ")
--- Programacion/Python/PrimerPrograma.py
+++
@@ -1,5 +0,0 @@
-numero1 = int(input("Introduce el primer numero: "))
-numero2 = int(input("Introduce el segundo numero: "))
-numero3 = int(input("Introduce el tercer numero: "))
-print("El numero mas alto es: {}".format(max(numero1, numero2, numero3)))
-print("El numero mas pequeño es: {}".format(min(numero1, numero2, numero3)))
